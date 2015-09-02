b4c4a9bac76f55187ffd7a66eaf72c0e5fd2feeb
vvv-v13 <vetal13@gmail.com>
2015-09-02 20:48:21 +0300

    Refactoring

--- python/tornado/app.py
+++ python/tornado/app.py
@@ -1,21 +1,22 @@
 import os
+import momoko
 
 import tornado.web
 import tornado.ioloop
-import tornado.options
+from tornado.options import define, options
+from tornado.web import RequestHandler, url
+
 import tornado.httpserver
 
 from tornado import gen
 from tornado.escape import json_encode
 
 
-import momoko
+define('port', default=8000, help='run on the given port', type=int)
+define('dsn', default='dbname=test user=test host=localhost port=5432', help='Database DSN', type=str)
 
 
-dsn = 'dbname=test user=test host=localhost port=5432'
-
-
-class BaseHandler(tornado.web.RequestHandler):
+class BaseHandler(RequestHandler):
     @property
     def db(self):
         return self.application.db
@@ -24,7 +25,23 @@ class BaseHandler(tornado.web.RequestHandler):
 class PSQLSelectHandler(BaseHandler):
     @gen.coroutine
     def get(self):
-        sql = """SELECT id, uid, bool_value, int_value, float_value, text_value, date_time, date, array_data, json_data FROM models LIMIT %s"""
+        sql = """
+                SELECT
+                        id,
+                        uid,
+                        bool_value,
+                        int_value,
+                        float_value,
+                        text_value,
+                        date_time,
+                        date,
+                        array_data,
+                        json_data
+                FROM
+                    models
+                LIMIT %s
+        """
+
         cur = yield self.db.execute(sql, (3,))
                
         data = []
@@ -48,40 +65,34 @@ class PSQLSelectHandler(BaseHandler):
 
 
 def main():
-    try:
-        tornado.options.parse_command_line()
-        application = tornado.web.Application([
-            (r'/psql/select', PSQLSelectHandler),
-        ], debug=False)
+    options.parse_command_line()
 
-        ioloop = tornado.ioloop.IOLoop.instance()
+    application = tornado.web.Application([
+        (r'/psql/select', PSQLSelectHandler),
+    ], debug=False)
 
-        application.db = momoko.Pool(
-            dsn=dsn,
-            size=1,
-            max_size=3,
-            ioloop=ioloop,
-            setsession=("SET TIME ZONE UTC",),
-            raise_connect_errors=False,
-        )
+    ioloop = tornado.ioloop.IOLoop.instance()
 
-        # this is a one way to run ioloop in sync
-        future = application.db.connect()
-        ioloop.add_future(future, lambda f: ioloop.stop())
-        ioloop.start()
+    application.db = momoko.Pool(
+        dsn=options.dsn,
+        size=1,
+        max_size=3,
+        ioloop=ioloop,
+    )
 
-        if application.db.server_version >= 90200:
-            future = application.db.register_json()
-            # This is the other way to run ioloop in sync
-            ioloop.run_sync(lambda: future)
+    # this is a one way to run ioloop in sync
+    future = application.db.connect()
+    ioloop.add_future(future, lambda f: ioloop.stop())
+    ioloop.start()
 
-        http_server = tornado.httpserver.HTTPServer(application)
-        http_server.listen(8000, 'localhost')
-        ioloop.start()
-
-    except KeyboardInterrupt:
-        print('Exit')
+    http_server = tornado.httpserver.HTTPServer(application)
+    http_server.listen(options.port, 'localhost')
+    ioloop.start()
 
 
 if __name__ == '__main__':
-    main()
+    try:
+        main()
+    except KeyboardInterrupt:
+        print('Exit')
+
